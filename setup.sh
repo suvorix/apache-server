d4a550a9cc884f58c4ddc91db409a5bd8706e564
Nikita <niksuvik@gmail.com>
2019-02-22 14:49:50 +0300

    new setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+apt-get update
 chmod +x ./ftp/install.sh
 ./ftp/install.sh
-apt-get -y install wget screen git
-git clone https://github.com/teddysun/lamp.git
-cd lamp
-chmod 755 *.sh
-screen -S lamp
-./lamp.sh
+apt-get -y install apache2
+systemctl enable apache2
+systemctl start apache2
+netstat -tulnp | grep apache2
+mkdir /data
+mkdir /data/default/www
+mkdir /data/default/logs
+rm /etc/logrotate.d/apache2
+cp ./conf/apache2 /etc/logrotate.d/
+apt-get -y install php
+apt-get -y install php-mysql php-mbstring php-xml php-gd php-gettext
+apt-get -y install mariadb-server mariadb-client
+mysql_secure_installation
+apt-get install -y phpmyadmin
+ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
+a2enconf phpmyadmin
+echo "update user set plugin='' where User='root'; flush privileges;" | mysql -u root -p mysql
+systemctl reload apache2
--- site_add.sh
+++ site_add.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-name=${1}
-dname=${2}
-useradd -d /data/www/${dname} -s /bin/bash -m ${name}
-passwd ${name}
-lamp add
-chown -R ${name}:${name} /data/www/${dname}
+read -e -p "User name: " name
+read -e -p "Server: " dname
+
+echo "${name} - ${dname}"
+
+echo "<VirtualHost *:80>"
+echo "	ServerName ${dname}"
+
+#echo "	ServerAlias www.${dname}"
+
+echo "	DocumentRoot /data/${dname}/www"
+echo "	<Directory /data/${dname}/www>"
+echo "		Options FollowSymLinks"
+echo "		AllowOverride All"
+echo "		Require all granted"
+echo "	</Directory>"
+echo "	ErrorLog /data/${dname}/logs/error.log"
+echo "	CustomLog /data/${dname}/logs/access.log common"
+echo "</VirtualHost>"
+echo "php_admin_value date.timezone 'Europe/Moscow'"
+echo "php_admin_value max_execution_time 60"
+echo "php_admin_value upload_max_filesize 30M"
+
+#useradd -d /data/www/${dname} -s /bin/bash -m ${name}
+#passwd ${name}
+
+#chown -R ${name}:${name} /data/www/${dname}

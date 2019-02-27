edc7037a0e2d9f5c1106a174cf1d35e91da80af8
Nikita <niksuvik@gmail.com>
2019-02-27 14:46:19 +0300

    Исправление багов при добавлении базы данных

--- add/db.sh
+++ add/db.sh
@@ -8,6 +8,5 @@ read -e -p "Имя базы данных: " ndb
 read -e -p "Имя пользователя базы данных: " udb
 read -e -p "Пароль пользователя базы данных: " pdb
 
-echo "create user '${udb}'@'localhost' identified by '${pdb}';" | mysql -u root -p mysql
-echo "create database ${ndb};" | mysql -u root -p mysql
-echo "grant all on ${ndb}.* to '${udb}'@'localhost'" | mysql -u root -p mysql
+echo "Введите пароль от root пользователя"
+echo "create user '${udb}'@'localhost' identified by '${pdb}'; create database ${ndb}; grant all on ${ndb}.* to '${udb}'@'localhost';" | mysql -u root -p mysql
